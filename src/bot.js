914e278695ca622c12bac293805813bbfa37d935
Rahul Prajapati <rrahulp395@gmail.com>
2022-06-12 23:47:20 +0530

    Started Adding Custom Commands

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,45 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+const INTENTS = [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+]
+const bot = new Client({ intents: INTENTS, partials: ['CHANNEL'] });
+const PREFIX = '$';
+const CUS_CMDS = ['kick','add','invite'];
 
-client.on('ready', async () => {
-    console.log(`${client.user.username, client.user.tag}`);
-    client.login(process.env.BOT_TOKEN);
+currentDate = () => {
+    date = new Date();
+    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+bot.on('ready', () => {
+    console.log(`[Logged In] --> ${currentDate()} --> ${bot.user.username, bot.user.tag}`);
 });
+
+bot.on('typingStart', (type) => {
+    console.log("Typing : ", type, type.author.tag);
+})
+
+bot.on('messageCreate', (message) => {
+    if (!message.author.bot){
+        if(message.content === 'hello') {
+            console.log('message', message.author.tag);
+            message.channel.send(`Hello ${message.author.tag}`);
+        }
+        else{
+            const [CMD, ...args] = message.content
+                                    .substring(PREFIX.length)
+                                    .split(/\s+/);
+            console.log(CMD, args);
+            if (CUS_CMDS.includes(CMD)) {
+                console.log('Gotcha');
+            }
+        }
+    }
+})
+
+bot.login(process.env.BOT_TOKEN);
